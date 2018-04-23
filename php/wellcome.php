3ffba33216726099d1aa07a8c118b6a06e556d79
iasynetska <viktoriia.iasynetska@gmail.com>
2018-04-23 14:40:23 +0200

    Adding links for login

--- php/wellcome.php
+++ php/wellcome.php
@@ -9,9 +9,9 @@
 		<title>Wellcome</title>
 	</head>
 	<body>
-		<a href="wellcome.php?lang=en"><?php echo $lang['wellcome_en']?></a>
-		<a href="wellcome.php?lang=pl"><?php echo $lang['wellcome_pl']?></a><br /><br />
-		<a href="#"><?php echo $lang['wellcome_btn_parent']?></a>
-		<a href="#"><?php echo $lang['wellcome_btn_kid']?></a>
+		<a href="wellcome.php?lang=en"><?php echo $lang['en']?></a>
+		<a href="wellcome.php?lang=pl"><?php echo $lang['pl']?></a><br /><br />
+		<a href="login_parent.php"><?php echo $lang['parent']?></a>
+		<a href="login_kid.php"><?php echo $lang['kid']?></a>
 	</body>
 </html>
